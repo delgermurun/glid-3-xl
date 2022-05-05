377c97dd51e63252df0de0213f30f7e90accec12
Han Xiao <han.xiao@jina.ai>
2022-05-05 22:29:26 +0200

    chore: first commit

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='dalle_flow_glid3',
+    py_modules=['dalle_flow_glid3'],
     packages=find_packages(),
     install_requires=["blobfile>=1.0.5"],
 )
